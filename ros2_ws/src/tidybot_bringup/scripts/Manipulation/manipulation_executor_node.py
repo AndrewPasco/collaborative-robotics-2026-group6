e8077d4db3d3dc5d020d6d5d41110b22413f7443
lukeyuen-ui <lukeyuen@stanford.edu>
2026-02-16 13:18:55 -0800

    Modify use_orientation in service request
    Change use_orientation to False for planning.

--- ros2_ws/src/tidybot_bringup/scripts/Manipulation/manipulation_executor_node.py
+++ ros2_ws/src/tidybot_bringup/scripts/Manipulation/manipulation_executor_node.py
@@ -529,7 +529,8 @@ class ManipulationExecutorNode(Node):
       request = PlanToTarget.Request()
       request.arm_name = self.arm_name
       request.target_pose = target_pose
-      request.use_orientation = True      # Use full 6-DOF IK
+      # request.use_orientation = True      # Use full 6-DOF IK
+      request.use_orientation = False
       request.execute = False             # Plan only, don't execute
       request.duration = MOVE_DURATION
       request.max_condition_number = 100.0
